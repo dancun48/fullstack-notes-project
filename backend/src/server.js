4e154ba5fb637a4d5712c2a5c43f1b4cf10690c5
dancun48 <111135378+dancun48@users.noreply.github.com>
2025-09-20 00:22:54 +0300

    deployment ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from 'path';
 
 // local imports
 import router from "./routes/notesRoutes.js";
@@ -13,14 +14,26 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware - a function that runs between the request and the response
-app.use(cors({ origin: "http://localhost:5173" }));    // allows all/specified origins to access this backend
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({ origin: "http://localhost:5173" }));    // allows all/specified origins to access this backend
+
+}
 app.use(express.json());    // parses the JSON bodies: req.body
 app.use(rateLimiter);   // will limit the number of requests made to the server to 100 per 60 seconds
 
 
 app.use("/api/notes", router);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+})
+}
+
 
 // ENDPOINT - is a combination of a URL+HTTP method that lets the client interact with a specific resource
 
